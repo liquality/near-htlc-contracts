0259db0f57ca03db18e174712f67ff58ebfbeb05
Krasimir Raykov <raykov@weichain.tech>
2021-05-06 21:49:19 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Near HTLC contracts
+
+# Quick Start
+
+To run compile and get the bytecode of the contract:
+
+1. npm i
+2. cd ./contract
+3. npm i
+4. cd ../
+5. npm run build
+6. the bytecode will be inside `./contract/bytecode.json`
---
+++ contract/assembly/index.ts
@@ -0,0 +1,49 @@
+import {
+  storage,
+  u256,
+  context,
+  math,
+  ContractPromiseBatch,
+} from "near-sdk-as";
+import { Swap } from "./model";
+
+export function init(
+  secretHash: Uint8Array,
+  expiration: u64,
+  buyer: string
+): void {
+  assert(!storage.hasKey("swap"), "ALREADY_INITIALIZED.");
+  assert(secretHash.length == 32, "INVALID_SECRET_HASH");
+  storage.set("swap", {
+    secretHash,
+    expiration,
+    buyer,
+    seller: context.sender,
+  } as Swap);
+}
+
+export function getSwap(): Swap {
+  return storage.getSome<Swap>("swap");
+}
+
+export function claim(secret: Uint8Array): void {
+  const swapInfo = getSwap();
+  assert(secret.length == 32, "INVALID_SECRET");
+  assert(
+    u256.fromUint8ArrayLE(math.sha256(secret)) ==
+      u256.fromUint8ArrayLE(swapInfo.secretHash),
+    "INVALID_SECRET"
+  );
+  assert(context.blockTimestamp <= swapInfo.expiration, "SWAP_EXPIRED");
+  ContractPromiseBatch.create(context.contractName).delete_account(
+    swapInfo.buyer
+  );
+}
+
+export function refund(): void {
+  const swapInfo = getSwap();
+  assert(context.blockTimestamp > swapInfo.expiration, "SWAP_NOT_EXPIRED");
+  ContractPromiseBatch.create(context.contractName).delete_account(
+    swapInfo.seller
+  );
+}
